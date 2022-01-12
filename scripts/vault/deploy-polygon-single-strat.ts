b5a526928d947de65b830c14a4b67a7743300943
pumpingGhost <pumpingGhost@byom.de>
2022-01-12 17:56:22 +0100

    add polygon single deploy script

--- scripts/vault/deploy-polygon-single-strat.ts
+++ scripts/vault/deploy-polygon-single-strat.ts
@@ -12,40 +12,39 @@ const {
   platforms: { quickswap, beefyfinance },
   tokens: {
     MATIC: { address: MATIC },
-    QUICK: { address: QUICK }
+    QUICK: { address: QUICK },
+    ETH: { address: ETH },
   },
 } = addressBook.polygon;
 
 const shouldVerifyOnEtherscan = false;
 
-const want = web3.utils.toChecksumAddress("0x3f245C6f18442Bd6198d964C567a01BD4202e290"); // LP addr
-const KIRO = web3.utils.toChecksumAddress("0xB382C1cfA622795a534e5bd56Fac93d59BAc8B0D");
-// const DQUICK = web3.utils.toChecksumAddress("0xf28164a485b0b2c90639e47b0f377b4a438a16b1")
+const want = web3.utils.toChecksumAddress("0x82ee4008E2DE03F3A3E25434506f0d4d423afaaD"); // LP addr
+const LMP = web3.utils.toChecksumAddress("0x873801Ae2ff12d816Db9a7B082F5796BEC64C82C");
 
 const vaultParams = {
-  mooName: "Moo QuickSwap KIRO-MATIC",
-  mooSymbol: "mooQuickSwapKIRO-MATIC",
+  mooName: "Moo QuickSwap LMP-ETH",
+  mooSymbol: "mooQuickSwapLMP-ETH",
   delay: 21600,
 };
 
 const strategyParams = {
   want,
-  rewardPool: "0xfF22Bf1f778BcD6741D823b077285533EC582F78",
+  rewardPool: "0x0997BA719cdF1F216d8A14b52AD3355Bd2F9f477",
   unirouter: quickswap.router,
   strategist: "0xc41Caa060d1a95B27D161326aAE1d7d831c5171E", // dev
   // keeper: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // test account
   keeper: beefyfinance.keeper,
   beefyFeeRecipient: beefyfinance.beefyFeeRecipient,
-  output0ToNativeRoute: [QUICK, MATIC], // ["0x831753DD7087CaC61aB5644b308642cc1c33Dc13", "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"]
-  output1ToNativeRoute: [KIRO, MATIC],
-  nativeToLp0Route: [MATIC],
-  nativeToLp1Route: [MATIC, KIRO],
+  outputToNativeRoute: [QUICK, MATIC], // ["0x831753DD7087CaC61aB5644b308642cc1c33Dc13", "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"]
+  outputToLp0Route: [QUICK, ETH],
+  outputToLp1Route: [QUICK, ETH, LMP],
   //pendingRewardsFunctionName: "pendingSushi", // used for rewardsAvailable(), use correct function name from masterchef
 };
 
 const contractNames = {
   vault: "BeefyVaultV6",
-  strategy: "StrategyQuickswapDualRewardLP",
+  strategy: "StrategyPolygonQuickLP",
 };
 
 async function main() {
@@ -86,10 +85,9 @@ async function main() {
     strategyParams.keeper,
     strategyParams.strategist,
     strategyParams.beefyFeeRecipient,
-    strategyParams.output0ToNativeRoute,
-    strategyParams.output1ToNativeRoute,
-    strategyParams.nativeToLp0Route,
-    strategyParams.nativeToLp1Route,
+    strategyParams.outputToNativeRoute,
+    strategyParams.outputToLp0Route,
+    strategyParams.outputToLp1Route,
   ];
   const strategy = await Strategy.deploy(...strategyConstructorArguments);
   await strategy.deployed();
